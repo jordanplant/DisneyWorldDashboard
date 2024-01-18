a3330d6175fa528760cb72d95bfe960a215b7dfc
Jordan Plant <jordanplant94@gmail.com>
2024-01-18 16:03:22 +0000

    first commit

--- src/Components/PageLayout.jsx
+++ src/Components/PageLayout.jsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 import "./PageLayout.css";
 import NavBar from "./NavBar";
 import Countdown from "./Countdown";
-import WeatherApp from "./Weatherapp";
+import WeatherApp from "./WeatherApp";
 import SnacksList from "./SnacksList";
 import Snacks from "./Snacks";
 import WaitTimes from "./WaitTimes";

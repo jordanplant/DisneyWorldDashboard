6eaf00b77eb52536fd7bd48b32d944685e4e4ffd
Jordan Plant <jordanplant94@gmail.com>
2024-04-07 23:35:22 +0100

    v1.3 - add itinerary

--- src/Components/Itinerary.jsx
+++ src/Components/Itinerary.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from "./itinerary.module.css";
+import styles from "./Itinerary.module.css";
 
 const Itinerary = () => {
   const [activeButton, setActiveButton] = useState(null);

4d6b3390b1c2e1310e218983b8999fd95c031f98
Jordan Plant <jordanplant94@gmail.com>
2024-04-29 16:19:45 +0100

    autoclose

--- src/Components/ButtonContainer.jsx
+++ src/Components/ButtonContainer.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./ButtonContainer.module.css";
 
 const ButtonContainer = ({ snack, handleEdit, handleDelete }) => {
   const [showEditButton, setShowEditButton] = useState(false);
   const [showDeleteButton, setShowDeleteButton] = useState(false);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target)
+      ) {
+        setShowEditButton(false);
+        setShowDeleteButton(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const handleMoreClick = () => {
-    setShowEditButton(prevState => !prevState);
-    setShowDeleteButton(prevState => !prevState);
+    setShowEditButton((prevState) => !prevState);
+    setShowDeleteButton((prevState) => !prevState);
   };
-  
 
   const handleEditClick = () => {
     handleEdit(snack);
@@ -20,9 +38,11 @@ const ButtonContainer = ({ snack, handleEdit, handleDelete }) => {
   };
 
   return (
-    <div className={styles.buttonContainer}>
+    <div className={styles.buttonContainer} ref={containerRef}>
       <button
-        className={`${styles.buttonEdit} ${showEditButton ? styles.showEdit : ''}`}
+        className={`${styles.buttonEdit} ${
+          showEditButton ? styles.showEdit : ""
+        }`}
         onClick={handleEditClick}
         disabled={!snack || snack.completed}
       >
@@ -30,12 +50,14 @@ const ButtonContainer = ({ snack, handleEdit, handleDelete }) => {
       </button>
 
       <button
-        className={`${styles.buttonDelete} ${showDeleteButton ? styles.showDelete : ''}`}
+        className={`${styles.buttonDelete} ${
+          showDeleteButton ? styles.showDelete : ""
+        }`}
         onClick={handleDeleteClick}
       >
         <i className="fas fa-trash fa-xs"></i>
       </button>
-    
+
       <button className={styles.buttonOption} onClick={handleMoreClick}>
         <i className="fa-solid fa-ellipsis"></i>
       </button>
